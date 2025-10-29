c04e37ba42597263b3eafd866296599871451943
Ruth Grace Wong <ruthgracewong@gmail.com>
2025-10-29 13:08:42 -0700

    update color picker - remove pattern print and specify tool description better for what to use it for

--- color_tools.py
+++ color_tools.py
@@ -19,7 +19,7 @@ def pick_random_color(user_id: str = None) -> dict:
         "Purple",
         "Black",
         "Pink",
-        "Multi-color / pattern print"
+        "Multi-color"
     ]
 
     chosen_color = random.choice(colors)
@@ -34,7 +34,7 @@ color_tools = [
     {
         "type": "custom",
         "name": "pick_random_color",
-        "description": "Pick a random color from a predefined list (Red, Orange, Yellow, Green, Blue, Purple, Black, Pink, or Multi-color/pattern print) and display it on the device screen.",
+        "description": "Pick a random color from a predefined list (Red, Orange, Yellow, Green, Blue, Purple, Black, Pink, or Multi-color/pattern print). Use this when the user is having trouble deciding on a color, for example for clothes to wear or buy, and doens't specify the color choices.",
         "input_schema": {
             "type": "object",
             "properties": {},
